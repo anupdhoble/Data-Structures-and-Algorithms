1d262a47da2d180a3c467dbc29e80c012a7a3900
anupdhoble <anupdhoble15@gmail.com>
2023-01-07 10:48:30 +0530

    Bttraversal.cpp updated,Is BST added.

---
+++ Binary Trees/BTtraversal.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+struct node{
+    struct node* left;
+    struct node* right;
+    int data;
+};
+
+struct node* createNode(int data){
+    struct node* n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+
+void printPreorder(struct node* root){
+    if(root!=NULL){
+        cout<<root->data;
+        printPreorder(root->left);
+        printPreorder(root->right);
+    }
+}
+void printPostorder(struct node* root){
+    if(root!=NULL){
+        printPostorder(root->left);
+        printPostorder(root->right);
+        cout<<root->data;
+    }
+}
+void printInorder(struct node* root){
+    if(root!=NULL){
+        printInorder(root->left);
+        cout<<root->data;
+        printInorder(root->right);
+    }
+}
+int main()
+{
+    cout<<"By Anup Dhoble..."<<'\a';
+     
+   struct node* p=createNode(7);
+   struct node* p1=createNode(6);
+   struct node* p3=createNode(0);
+   struct node* p4=createNode(4);
+   struct node* p2=createNode(1);
+
+     // Finally The tree looks like this:
+    //      7
+    //     / \
+    //    6   1
+    //   / \
+    //  0   4 
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    cout<<"\nPreorder: ";
+    printPreorder(p);
+    cout<<"\nPostorder: ";
+    printPostorder(p);
+    cout<<"\nInorder: ";
+    printInorder(p);
+    return 0;
+}
--- Binary Trees/IsBST.cpp
+++ Binary Trees/IsBST.cpp
@@ -23,27 +23,44 @@ void printPreorder(struct node* root){
         printPreorder(root->right);
     }
 }
+void printPostorder(struct node* root){
+    if(root!=NULL){
+        printPostorder(root->left);
+        printPostorder(root->right);
+        cout<<root->data;
+    }
+}
+void printInorder(struct node* root){
+    if(root!=NULL){
+        printInorder(root->left);
+        cout<<root->data;
+        printInorder(root->right);
+    }
+}
 int main()
 {
     cout<<"By Anup Dhoble..."<<'\a';
      
-   struct node* p=createNode(7);
-   struct node* p1=createNode(6);
+   struct node* p=createNode(5);
+   struct node* p1=createNode(3);
    struct node* p3=createNode(0);
    struct node* p4=createNode(4);
-   struct node* p2=createNode(1);
+   struct node* p2=createNode(6);
 
-     // Finally The tree looks like this:
-    //      7
+     // Finally The BST tree looks like this:
+    //      5
     //     / \
-    //    6   1
+    //    3   6
     //   / \
     //  0   4 
     p->left=p1;
     p->right=p2;
     p1->left=p3;
     p1->right=p4;
-    cout<<"\nPreorder: ";
-    printPreorder(p);
+    
+    cout<<"\nInorder: ";
+    printInorder(p);
+    //If Inorder is in ascending order then BT is BST.
+
     return 0;
 }
