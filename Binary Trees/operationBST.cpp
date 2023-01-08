5fce5c8c669359018d0628a4a8cf503316dd45f1
anupdhoble <anupdhoble15@gmail.com>
2023-01-08 12:49:58 +0530

    nothing

--- Binary Trees/operationBST.cpp
+++ Binary Trees/operationBST.cpp
@@ -1,137 +1,129 @@
-#include<bits/stdc++.h>
-#include<malloc.h>
+#include <bits/stdc++.h>
+#include <malloc.h>
 
 using namespace std;
-struct node{
-    struct node* left;
-    struct node* right;
+struct node
+{
+    struct node *left;
+    struct node *right;
     int data;
 };
-
-struct node* createNode(int data){
-    struct node* n;
-    n=(struct node*)malloc(sizeof(struct node));
-    n->data=data;
-    n->left=NULL;
-    n->right=NULL;
+struct node *createNode(int data)
+{
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
     return n;
 }
-
-
-void printInorder(struct node* root){
-    if(root!=NULL){
+void printInorder(struct node *root)
+{
+    if (root != NULL)
+    {
         printInorder(root->left);
-        cout<<root->data;
+        cout << root->data;
         printInorder(root->right);
     }
 }
-int isBST(struct node* root){
-    static struct node *prev=NULL;
-    if(root!=NULL){
-        if(!isBST(root->left)){
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
             return 0;
         }
-        if(prev!=NULL&&root->data<=prev->data){
+        if (prev != NULL && root->data <= prev->data)
+        {
             return 0;
         }
-        prev=root;
+        prev = root;
         return isBST(root->right);
     }
-    else{
+    else
+    {
         return 1;
     }
 }
-struct node* searchRec(struct node* root,int key){
-    if(root==NULL)
-        return NULL;
-    if(root->data>key)
-        return searchRec(root->left,key);
-    else if(root->data==key)
-            return root;
-        else
-            return searchRec(root->right,key);
-}
-//Iterative Solution
-struct node* searchItr(struct node* root,int key){
-    while(root!=NULL){
-        if(key==root->data)
-            return root;
-        else if(key<root->data)
-            root=root->left;
-        else{
-            root=root->left;
-        }
-    }
-    return NULL;
-}
-//  
-void insert_bst(struct node* root,int key){
-    struct node* prev =NULL;
-    while(root!=NULL){
-        prev=root;
-        if(key==root->data){
-            cout<<"Error:- Insertion failed,value already exist in BST..";
+
+void insert_bst(struct node *root, int key)
+{
+    struct node *prev = NULL;
+    while (root != NULL)
+    {
+        prev = root;
+        if (key == root->data)
+        {
+            cout << "Error:- Insertion failed,value already exist in BST..";
             return;
         }
-        else if(root->data>key){
-                root=root->left;
-            }
-        else{
-            root=root->right;
+        else if (root->data > key)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
         }
     }
-        struct node* n =createNode(key);
-        if(key<prev->data){
-            prev->left=n;
-        }
-        else{
-            prev->right=n;
-        }
-    
+    struct node *n = createNode(key);
+    if (key < prev->data)
+    {
+        prev->left = n;
+    }
+    else
+    {
+        prev->right = n;
+    }
 }
 int main()
 {
-    cout<<"By Anup Dhoble..."<<'\a';
-     
-   struct node* p=createNode(6);
-   struct node* p1=createNode(3);
-   struct node* p2=createNode(8);
-   struct node* p3=createNode(1);
-   struct node* p4=createNode(4);
+    cout << "By Anup Dhoble..." << '\a';
 
-     // Finally The BST tree looks like this:
+    struct node *p = createNode(6);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(8);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+
+    // Finally The BST tree looks like this:
     //      6
     //     / \
     //    3   8
     //   / \
-    //  1   4 
-    //Eg. After insertion of 2 BST looks like
+    //  1   4
+    // Eg. After insertion of 2 BST looks like
     //      6
     //     / \
     //    3   8
     //   / \
-    //  1   4 
+    //  1   4
     // / \
     //[]  2
-    p->left=p1;
-    p->right=p2;
-    p1->left=p3;
-    p1->right=p4;
-    
-    cout<<"\nInorder: ";
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    cout << "\nInorder: ";
     printInorder(p);
-    //If Inorder is in ascending order then BT is BST.
-    if(isBST(p)){
-        cout<<"\nIt is a Binary Search Tree..";
+    // If Inorder is in ascending order then BT is BST.
+    if (isBST(p))
+    {
+        cout << "\nIt is a Binary Search Tree..";
     }
-    else{
-        cout<<"\nIt is not a Binary Search Tree..";
+    else
+    {
+        cout << "\nIt is not a Binary Search Tree..";
     }
     // int key;
     // cout<<"\nEnter key to be inserted: ";
     // cin>>key;
-    insert_bst(p,2);
-    cout<<"\n"<<p->left->left->right->data;
-    
+    insert_bst(p, 2);
+    cout << "\n"
+         << p->left->left->right->data;
+
     return 0;
 }
